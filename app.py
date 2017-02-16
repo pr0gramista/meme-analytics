e2e52acffc7e91b640241c4449747a72dd4ee0e2
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-02-16 22:28:37 +0100

    Elasticsearch introduced

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 import configparser
 import requests
 import json
+from elasticsearch import Elasticsearch
 
 print("Meme analytics running")
 
@@ -14,6 +15,8 @@ api = 'http://localhost:8080'
 
 # Elasticsearch connection string
 es_conn = None
+es_index = ''
+es = None
 
 def read_config():
     config = configparser.ConfigParser()
@@ -21,6 +24,8 @@ def read_config():
     global sites, es_conn, api
     sites = config.get('main', 'sites', fallback=sites).split(',')
     es_conn = config.get('main', 'es_conn', fallback=es_conn)
+    if es_conn is not None:
+        es_conn = json.loads(es_conn)
     api = config.get('main', 'api', fallback=api)
 
 def print_config():
@@ -29,11 +34,16 @@ def print_config():
     if es_conn is not None:
         print('Elasticsearch connection url: ' + es_conn)
 
-def get_memes(site):
-    url = api + '/' + site
+def get_memes(site, page):
+    fail = False # Whether it should stop fetching memes
+
+    if page == 0:
+        url = api + '/' + site
+    else:
+        url = api + '/' + site + '/' + str(page)
 
     data = requests.get(url).json()
-    print(data)
+    return data['memes']
 
 def scan():
     print("I'm working...")
@@ -41,8 +51,26 @@ def scan():
 read_config()
 print_config()
 
+es = Elasticsearch(hosts=es_conn)
+
 for site in sites:
-    get_memes(site)
+    fail = False
+    page = 0
+    while fail is False:
+        memes = get_memes(site, page)
+
+        for meme in memes:
+            # if is_new:
+            es.index(index="test-index", doc_type='meme', body=meme)
+            print(meme)
+            # else
+            #   break or something like that
+
+        page += 1
+        if page > 20:
+            fail = True
+
+
 schedule.every(5).seconds.do(scan)
 
 while True:
