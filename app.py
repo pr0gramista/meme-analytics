49c7462657fa91ce00ce1297951b1c68cf8c96cb
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-02-17 17:29:09 +0100

    Old memes won't be taken for indexing

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ import time
 import configparser
 import requests
 import json
+import os
 from elasticsearch import Elasticsearch
 
 print("Meme analytics running")
+
+# Last memes, used to determine whether meme is new
+last_memes = {}
+last_memes_directory = "last_memes"
 
 # Sites to scan
 sites = []
@@ -20,6 +25,22 @@ api = 'http://localhost:8080'
 es_conn = None
 es_index = 'test-index'
 es = None
+
+def save_last_memes(site, memes):
+    print("Saving last memes for site {}. Memes are: {}".format(site, memes))
+    path = last_memes_directory + "/" + site + ".json"
+    os.makedirs(last_memes_directory, exist_ok=True)
+    with open(path, 'w+') as f:
+        json.dump(memes, f, sort_keys=True, indent=4, separators=(',', ': '))
+
+def load_last_memes():
+    global last_memes
+    for site in sites:
+        path = last_memes_directory + "/" + site + ".json"
+        if os.path.isfile(path):
+            print("Loading last memes for site {}".format(site))
+            with open(path, 'r') as f:
+                last_memes[site] = json.load(f)
 
 def read_config():
     config = configparser.ConfigParser()
@@ -40,10 +61,15 @@ def print_config():
         print('Elasticsearch connection url: ' + es_conn)
 
 def is_new(site, meme):
-    return True #dummy code
+    if site in last_memes:
+        for last_meme in last_memes[site]:
+            if last_meme == meme:
+                return False
+    return True
 
 def scan():
     print("Scanning...")
+    load_last_memes()
     for site in sites:
         print("Site: " + site)
         page = "/" + site
@@ -54,6 +80,11 @@ def scan():
             page_count += 1
 
             data = requests.get(url).json()
+
+            if page_count is 1:
+                # Save first 3 memes
+                # these won't be taken under check in this scan
+                save_last_memes(site, data['memes'][0:3])
 
             for meme in data['memes']:
                 print(site + " meme: " + meme['title'])
@@ -80,7 +111,7 @@ es = Elasticsearch(hosts=es_conn)
 scan()
 
 print("Standby mode")
-schedule.every(15).minutes.do(scan)
+schedule.every(15).seconds.do(scan)
 
 while True:
     schedule.run_pending()
